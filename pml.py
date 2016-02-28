55ef686006e3bef1c5184522d0da584c9397ff1a
Stuart Morgan <stuart.morgan@mwrinfosecurity.com>
2016-02-28 17:11:20 +0000

    Fixed whitespace

--- pml.py
+++ pml.py
@@ -12,23 +12,23 @@ def process_input(sql,lines):
     main_count = 0 
     num_lines = len(lines)
     current = {}
-	for line in lines:
-	
-	    main_count += 1
-	    percentage_count = "{0:.0f}%".format(float(main_count)/num_lines * 100)
-	    sys.stdout.write("\r  Processing line "+str(main_count)+"/"+str(num_lines)+" ("+percentage_count+")")
-	    sys.stdout.flush()
-	    
-	    val = re.match('^\s*\*\s+Username\s+:\s+(.+)\s*$', line.strip())
-	    if val != None:
-	        insert_into_db(sql,current)
-	        current = {}
-	        current['Username'] = val.group(1).strip()
-	        continue
-	
-	    val = re.match('^\s*\*\s+(Domain|NTLM|SHA1|Password)\s+:\s+(.+)\s*$', line.strip())
-	    if val != None:
-	        current[val.group(1).strip()] = unicode(val.group(2), errors='ignore')
+    for line in lines:
+    
+        main_count += 1
+        percentage_count = "{0:.0f}%".format(float(main_count)/num_lines * 100)
+        sys.stdout.write("\r  Processing line "+str(main_count)+"/"+str(num_lines)+" ("+percentage_count+")")
+        sys.stdout.flush()
+        
+        val = re.match('^\s*\*\s+Username\s+:\s+(.+)\s*$', line.strip())
+        if val != None:
+            insert_into_db(sql,current)
+            current = {}
+            current['Username'] = val.group(1).strip()
+            continue
+    
+        val = re.match('^\s*\*\s+(Domain|NTLM|SHA1|Password)\s+:\s+(.+)\s*$', line.strip())
+        if val != None:
+            current[val.group(1).strip()] = unicode(val.group(2), errors='ignore')
 
 # Build the SQL database schema
 def build_db_schema(sql):
@@ -64,36 +64,36 @@ def insert_into_db(sql,current):
 
 def create_db(filename):
     if filename == '':
-	    db_file = tempfile.NamedTemporaryFile(delete=False)
-	    db_filename = db_file.name+'.'+time.strftime('%Y%m%d%H%M%S')+'.mimikatz.db'
-	    db_file.close()
+        db_file = tempfile.NamedTemporaryFile(delete=False)
+        db_filename = db_file.name+'.'+time.strftime('%Y%m%d%H%M%S')+'.mimikatz.db'
+        db_file.close()
     else:
         db_filename = filename
 
-	sql = sqlite3.connect(filename)
-	build_db_schema(sql)
+    sql = sqlite3.connect(filename)
+    build_db_schema(sql)
     return sql
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser()
-	parser.add_argument('--database', action='store', help='The location of the SQLite database. If this is not specified, a new one will be created. If it is specified and the file exists, it will be opened and used. If a name is specified that does not exist, it will be created.')
-	parser.add_argument('--input', action='store', help='The mimikatz log file to read. If not specified, STDIN will be used.')
-	args = parser.parse_args()
-	
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--database', action='store', help='The location of the SQLite database. If this is not specified, a new one will be created. If it is specified and the file exists, it will be opened and used. If a name is specified that does not exist, it will be created.')
+    parser.add_argument('--input', action='store', help='The mimikatz log file to read. If not specified, STDIN will be used.')
+    args = parser.parse_args()
+    
     if 'database' in args:
         if os.path.isfile(args['database']:
             sql, db_filename = sqlite3.connect(args['database'])
-	        sys.stdout.write("Opening database: "+db_filename+"\n")
+            sys.stdout.write("Opening database: "+db_filename+"\n")
         else:
-	        sys.stdout.write("New database created: "+db_filename+"\n")
+            sys.stdout.write("New database created: "+db_filename+"\n")
             sql, db_filename = create_db(args['database'])
     else:
-	    sys.stdout.write("No database file specified; new database created: "+db_filename+"\n")
+        sys.stdout.write("No database file specified; new database created: "+db_filename+"\n")
         sql, db_filename = create_db(args['database'])
 
-	lines = sys.stdin.readlines()
-	process_input(lines)
-	
-	sql.close()
-	sys.exit(0)
+    lines = sys.stdin.readlines()
+    process_input(lines)
+    
+    sql.close()
+    sys.exit(0)
